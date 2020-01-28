d132c655f50af29c431e871998d8a853dd3b96a0
antoningirard <antonin.girard.98@gmail.com>
2020-01-28 14:22:20 +0100

    classe voiture -class coupe -class vehicule -voiture extends vehicule -coupe extends voiture

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -1,13 +1,7 @@
 <?php
 
-class Voiture
+class Voiture extends Vehicule
 {
-    public $marque;
-    public $modele;
-    public $couleur;
-    public $dimensions = [0, 0, 0];
-    public $masse;
-    public $vitesse = 0;
 
     public function __construct(string $mar, string $mod, string $cou, array $dim, int $mas)
     {
@@ -16,6 +10,20 @@ class Voiture
         $this->couleur = $cou;
         $this->dimensions = $dim;
         $this->masse = $mas;
+    }
+
+    public function getMarque()
+    {
+        return $this->marque;
+    }
+
+    public function setMarque($m) : void
+    {
+        $this->marque = $m;
+    }
+    public function calculEnergetiqueCinetique() : float
+    {
+        return 0.5 * $this->masse * $this->vitesse ** 2;
     }
 
     public function afficherMessage(){
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
+require "classes/Vehicule.php";
 require "classes/Voiture.php";
+require "classes/Coupe.php";
+
+
+$toto = new Coupe("peugeot", "504", "vert", [2552, 24526,26], 1200);
+var_dump($toto);
 
 $voiture = new Voiture("lada", "niva", "blanc", [2000, 1500, 800],1000);
 echo '<pre>';
@@ -9,10 +15,16 @@ echo '</pre>';
 
 $voiture->afficherMessage();
 
-$voiture = new Voiture();
-
-$voiture->modele = "RB14";
-$voiture->couleur = "Rouge";
-$voiture->dimensions = [2000, 1000, 500];
-
 echo $voiture->couleur;
+
+$voiture1 = new Voiture("Lada", "Niva", "blanc", [2000, 1500, 800], 900);
+$voiture2 = new Voiture("Nissan", "Cube", "violet", [2500, 1800, 1200], 1200);
+$voiture1->vitesse = 25;
+$voiture2->vitesse = 30;
+
+echo $voiture1->getMarque();
+
+
+//echo $voiture1->calculEnergetiqueCinetique() . "<br/>";
+//$voiture1->vitesse = 20;
+//echo $voiture1->calculEnergetiqueCinetique();
