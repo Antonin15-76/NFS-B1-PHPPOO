7b1242fa9264eee83c0d484a89e4b110a08da4f5
antoningirard <antonin.girard.98@gmail.com>
2020-01-28 11:37:16 +0100

    classe voiture -definition des proprieté -fonction construct

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -8,4 +8,17 @@ class Voiture
     public $dimensions = [0, 0, 0];
     public $masse;
     public $vitesse = 0;
+
+    public function __construct(string $mar, string $mod, string $cou, array $dim, int $mas)
+    {
+        $this->marque = $mar;
+        $this->modele = $mod;
+        $this->couleur = $cou;
+        $this->dimensions = $dim;
+        $this->masse = $mas;
+    }
+
+    public function afficherMessage(){
+        echo "je suis Michel";
+    }
 }
--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+
+require "classes/Voiture.php";
+
+$voiture = new Voiture("lada", "niva", "blanc", [2000, 1500, 800],1000);
+echo '<pre>';
+var_dump($voiture);
+echo '</pre>';
+
+$voiture->afficherMessage();
+
+$voiture = new Voiture();
+
+$voiture->modele = "RB14";
+$voiture->couleur = "Rouge";
+$voiture->dimensions = [2000, 1000, 500];
+
+echo $voiture->couleur;
